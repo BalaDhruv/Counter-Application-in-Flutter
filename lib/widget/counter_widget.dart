032325601a7bfe41253a07625f62b96f150e1274
BalaDhruv <androbalamail@gmail.com>
2018-12-10 15:15:01 +0530

    Simple bloc developed

--- lib/widget/counter_widget.dart
+++ lib/widget/counter_widget.dart
@@ -15,65 +15,86 @@ class _CounterWidgetState extends State<CounterWidget> {
   int count = 0;
   Color color = CounterTheme.primaryColor;
 
+  CounterBloc bloc;
+  var myCounter ;
+
+  @override
+  void initState() {
+    super.initState();
+    myCounter = CounterBlocTheme(count: 0, color: CounterTheme.primaryColor);
+    bloc = CounterBloc(myCounter);
+  }
+
+  @override
+  void dispose() {
+    print('releasing memory..');
+    bloc.close();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    var myCounter =
-        CounterBlocTheme(count: 0, color: CounterTheme.primaryColor);
-    CounterBloc bloc = CounterBloc();
     return CounterBlocProvider(
       bloc: bloc,
-      child: StreamBuilder<Color>(
-        stream: bloc.color,
-        builder: (context,snapshotColor)=>Scaffold(
-          appBar: AppBar(
-            backgroundColor: snapshotColor.data,
-            brightness: Brightness.dark,
-            centerTitle: true,
-            elevation: 0.0,
-            title: Text('Counter'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                StreamBuilder<int>(
-                  stream: bloc.count,
-                  builder: (context,snapshot) => Text(
-                    '${snapshot.data}',
-                    style:
-                    Theme.of(context).textTheme.display4.copyWith(color: snapshotColor.data),
-                  ),
-                ),
-                SizedBox(
-                  height: 50.0,
-                ),
-                RaisedButton(
-                  padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 40.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(8.0),
-                    ),
-                  ),
-                  color: snapshotColor.data,
-                  child: Text(
-                    'Click to Add',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {
-                    bloc.onAdd(myCounter);
-                    // setState(() {
-                    //   count++;
-                    //   color = CounterTheme.colorsList[Random().nextInt(5)];
-                    // });
-                  },
-                ),
-              ],
+      child: StreamBuilder<CounterBlocTheme>(
+        stream: bloc.themeObject,
+        initialData: myCounter,
+        builder: (context,themeSnapshot){
+          if(themeSnapshot.hasData){
+            print(themeSnapshot.data.color.toString());
+          }else{
+            print("No data available");
+          }
+          return themeSnapshot.hasData ? Scaffold(
+            appBar: AppBar(
+              backgroundColor: themeSnapshot.data.color,
+              brightness: Brightness.dark,
+              centerTitle: true,
+              elevation: 0.0,
+              title: Text('Counter'),
             ),
-          ),
-        ),
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    '${themeSnapshot.data.count}',
+                    style:
+                    Theme.of(context).textTheme.display4.copyWith(color: themeSnapshot.data.color),
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  RaisedButton(
+                    padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 40.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
+                      ),
+                    ),
+                    color: themeSnapshot.data.color,
+                    child: Text(
+                      'Click to Add',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      myCounter.updateCounterTheme();
+                      bloc.onAdd(myCounter);
+                      // setState(() {
+                      //   count++;
+                      //   color = CounterTheme.colorsList[Random().nextInt(5)];
+                      // });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ): Container(color: Colors.red,);
+        }
       ),
     );
   }
