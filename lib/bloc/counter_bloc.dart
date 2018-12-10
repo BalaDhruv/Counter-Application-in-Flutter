3449271c8ade2134ebcec9fa27816bb67d166eda
BalaDhruv <androbalamail@gmail.com>
2018-12-10 14:22:40 +0530

    with stream bloc update

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,6 +1,27 @@
+import 'dart:async';
+import 'package:counter_app_flutter/model/counter_bloc_theme.dart';
+import 'package:flutter/material.dart';
 
+class CounterBloc {
+  final blocController = StreamController<CounterBlocTheme>();
 
-//class CounterBloc{
-//
-//  final
-//}
+  Sink<CounterBlocTheme> get change => blocController.sink;
+
+  final countController = StreamController<int>();
+
+  Stream<int> get count => countController.stream;
+
+  final colorController = StreamController<Color>();
+
+  Stream<Color> get color => colorController.stream;
+
+  CounterBloc() {
+    blocController.stream.listen(onAdd);
+  }
+
+  void onAdd(CounterBlocTheme bloc) {
+    bloc.updateCounterTheme();
+    countController.add(bloc.count);
+    colorController.add(bloc.color);
+  }
+}
--- lib/widget/counter_widget.dart
+++ lib/widget/counter_widget.dart
@@ -1,7 +1,9 @@
+import 'package:counter_app_flutter/bloc/counter_bloc.dart';
+import 'package:counter_app_flutter/bloc/counter_bloc_provider.dart';
+import 'package:counter_app_flutter/model/counter_bloc_theme.dart';
 import 'package:counter_app_flutter/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 class CounterWidget extends StatefulWidget {
   @override
@@ -15,49 +17,62 @@ class _CounterWidgetState extends State<CounterWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: color,
-        brightness: Brightness.dark,
-        centerTitle: true,
-        elevation: 0.0,
-        title: Text('Counter'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              '$count',
-              style:
-                  Theme.of(context).textTheme.display4.copyWith(color: color),
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            RaisedButton(
-              padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 40.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(8.0),
+    var myCounter =
+        CounterBlocTheme(count: 0, color: CounterTheme.primaryColor);
+    CounterBloc bloc = CounterBloc();
+    return CounterBlocProvider(
+      bloc: bloc,
+      child: StreamBuilder<Color>(
+        stream: bloc.color,
+        builder: (context,snapshotColor)=>Scaffold(
+          appBar: AppBar(
+            backgroundColor: snapshotColor.data,
+            brightness: Brightness.dark,
+            centerTitle: true,
+            elevation: 0.0,
+            title: Text('Counter'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                StreamBuilder<int>(
+                  stream: bloc.count,
+                  builder: (context,snapshot) => Text(
+                    '${snapshot.data}',
+                    style:
+                    Theme.of(context).textTheme.display4.copyWith(color: snapshotColor.data),
+                  ),
                 ),
-              ),
-              color: color,
-              child: Text(
-                'Click to Add',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
+                SizedBox(
+                  height: 50.0,
                 ),
-              ),
-              onPressed: () {
-                setState(() {
-                  count++;
-                  color = CounterTheme.colorsList[Random().nextInt(5)];
-                });
-              },
+                RaisedButton(
+                  padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 40.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8.0),
+                    ),
+                  ),
+                  color: snapshotColor.data,
+                  child: Text(
+                    'Click to Add',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    bloc.onAdd(myCounter);
+                    // setState(() {
+                    //   count++;
+                    //   color = CounterTheme.colorsList[Random().nextInt(5)];
+                    // });
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
